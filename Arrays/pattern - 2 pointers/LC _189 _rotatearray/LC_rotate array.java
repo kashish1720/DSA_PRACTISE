648d4be7f8dfde5b66cae8ce52f4166197bb76ae
KASHISH CHHABADIYA <chhabadiya1712@gmail.com>
2026-09-05 14:36:36 +0530

    Update and rename Arrays/pattern - 2 pointers/LC - 189 . Rotate array to Arrays/pattern - 2 pointers/LC _189 _rotatearray/LC_rotate array.java

---
+++ Arrays/pattern - 2 pointers/LC _189 _rotatearray/LC_rotate array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int []result = new int [nums.length];
+        for(int i = 0;i<nums.length;i++){
+            int newposition = (i+k)%nums.length;
+            result[newposition] = nums[i];
+        }
+        for(int i =0;i<nums.length;i++){
+            nums[i]= result[i];
+        }
+        
+    }
+}
+
+////LC LINK - https://leetcode.com/problems/rotate-array/

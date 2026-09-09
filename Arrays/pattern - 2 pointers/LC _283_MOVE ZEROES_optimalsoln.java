94d8b8412435909684a801c6837f316090b8600b
KASHISH CHHABADIYA <chhabadiya1712@gmail.com>
2026-09-09 19:44:45 +0530

    Create LC _283_MOVE ZEROES_optimalsoln.java

---
+++ Arrays/pattern - 2 pointers/LC _283_MOVE ZEROES_optimalsoln.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int n = arr.length;
+        int j = -1;
+        for(int i = 0;i<n;i++){
+            if(arr[i]==0){
+                j =i;
+                break;
+            }
+        }
+        for(int i = j+1;i<n;i++){
+            if(arr[i]!= 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        
+    }
+}
+
+
+//question link - https://leetcode.com/problems/move-zeroes/
+//video link - https://www.youtube.com/watch?v=wvcQg43_V8U&t=1633s
